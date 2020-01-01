c7cb4adb24b2790caa2131a1fa37886736aa7120
nan-noo <uni613@naver.com>
2020-01-01 20:06:07 +0900

    Update AppGlideModule.kt

--- app/src/main/java/com/example/basictabkt/helper/AppGlideModule.kt
+++ app/src/main/java/com/example/basictabkt/helper/AppGlideModule.kt
@@ -2,5 +2,6 @@ package com.example.basictabkt.helper
 
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
+
 @GlideModule
 class AppGlideModule : AppGlideModule()
