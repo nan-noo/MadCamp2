d09967e6d3ab0c77c4d13bb45578db5b9dec1720
tmddus49 <oox1987@kaist.ac.kr>
2020-01-01 20:53:39 +0900

    milliseconds

--- app/src/main/java/com/example/basictabkt/Tab3Fragment.kt
+++ app/src/main/java/com/example/basictabkt/Tab3Fragment.kt
@@ -1,13 +1,17 @@
 package com.example.basictabkt
 
 import android.os.Bundle
+import android.os.Handler
 import android.os.SystemClock
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Chronometer
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.os.postDelayed
 import androidx.fragment.app.Fragment
 
 
@@ -22,7 +26,8 @@ class Tab3Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_tab3, container, false)
-
+        val milli = view.findViewById<TextView>(R.id.milliseconds)
+        val handler = Handler()
 
         chronometer = view.findViewById(R.id.chronometer)
         chronometer!!.format = "%s"
@@ -30,17 +35,29 @@ class Tab3Fragment : Fragment() {
 
         chronometer!!.onChronometerTickListener =
             Chronometer.OnChronometerTickListener { chronometer ->
-                if (SystemClock.elapsedRealtime() - chronometer.base >= 100000) {
+                if (SystemClock.elapsedRealtime() - chronometer.base >= 10000000) {
+                    Log.d("elapsedtime", (SystemClock.elapsedRealtime() - chronometer.base).toString())
                     chronometer.base = SystemClock.elapsedRealtime()
                     Toast.makeText(context, "OutOfBound!", Toast.LENGTH_SHORT).show()
                 }
             }
+
+        var runnable: Runnable? = null
+        runnable = Runnable {
+            if (running) {
+                val millisecondsTime = SystemClock.uptimeMillis() - chronometer!!.base
+                val milliseconds = (millisecondsTime % 1000).toInt() / 10
+                milli.text = String.format(".%02d", milliseconds)
+                handler.postDelayed(runnable!!, 0)
+            }
+        }
 
         val button1 = view.findViewById(R.id.start) as Button
         button1.setOnClickListener {
             if (!running) {
                 chronometer!!.base = SystemClock.elapsedRealtime() - pauseOffset
                 chronometer!!.start()
+                handler.postDelayed(runnable, 0)
                 running = true
 
             }
@@ -51,6 +68,7 @@ class Tab3Fragment : Fragment() {
             if (running) {
                 chronometer!!.stop()
                 pauseOffset = SystemClock.elapsedRealtime() - chronometer!!.base
+                handler.removeCallbacks(runnable)
                 running = false
             }
         }
@@ -68,6 +86,7 @@ class Tab3Fragment : Fragment() {
                 pauseOffset = 0
             }
         }
+
 
         return view
     }
