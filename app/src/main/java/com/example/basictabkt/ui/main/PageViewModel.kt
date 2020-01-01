6e84dbf6349b54fea3c7c45d10f63b08f15e50b7
nan-noo <uni613@naver.com>
2020-01-01 18:41:06 +0900

    Update PageViewModel.kt

--- app/src/main/java/com/example/basictabkt/ui/main/PageViewModel.kt
+++ app/src/main/java/com/example/basictabkt/ui/main/PageViewModel.kt
@@ -1,10 +1,7 @@
 package com.example.basictabkt.ui.main
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 
 class PageViewModel : ViewModel() {
 
